70531df3e39fef7bb5ff84050e346ecae73e56f6
Denys Kurylenko <lightcom2001@gmail.com>
2022-08-27 15:39:00 +0300

    added favorites list feature

---
+++ src/components/Favorites.tsx
@@ -0,0 +1,92 @@
+import './favorites.scss';
+import { useEffect, useState } from 'react';
+
+const Favorites = ({ showFavorites, setShowFavorites, setNickname }: any) => {
+	const [favorites, setFavorites] = useState<any[]>([]);
+
+	useEffect(() => {
+		const favoritesLC = JSON.parse(localStorage.getItem('favorites')!) || [];
+		setFavorites(favoritesLC);
+	}, []);
+
+	const handleFavoritesClick = (name: string) => {
+		setNickname(name);
+		setShowFavorites(!showFavorites);
+	};
+
+	const handleDelete = (id: number) => {
+		const filteredFavorites = favorites.filter(player => player.id !== id);
+
+		localStorage.setItem('favorites', JSON.stringify([...filteredFavorites]));
+		setFavorites([...filteredFavorites]);
+	};
+
+	return (
+		<div className='favorites-modal'>
+			<div
+				className='close-btn'
+				onClick={() => setShowFavorites(!showFavorites)}>
+				<svg
+					width='40'
+					height='40'
+					viewBox='0 0 40 40'
+					fill='none'
+					xmlns='http://www.w3.org/2000/svg'>
+					<path
+						d='M11 28.8926L28.8926 11'
+						stroke='white'
+						strokeWidth='1.5'
+						strokeLinecap='round'
+					/>
+					<path
+						d='M11 11L28.8926 28.8926'
+						stroke='white'
+						strokeWidth='1.5'
+						strokeLinecap='round'
+					/>
+				</svg>
+			</div>
+			<div className='favorites-names'>
+				{favorites.length < 1 && 'Favorites list is empty'}
+				{favorites.map(player => (
+					<div className='favorites-wrapper' key={player.id}>
+						<p
+							className='favorites-wrapper--name'
+							onClick={() => handleFavoritesClick(player.name)}>
+							{player.name}
+						</p>
+						<span className='delete' onClick={() => handleDelete(player.id)}>
+							<svg
+								width='30'
+								height='30'
+								viewBox='0 0 30 30'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'>
+								<path
+									d='M12.5 2.8125C11.9822 2.8125 11.5625 3.23223 11.5625 3.75V4.6875H6.25C5.73223 4.6875 5.3125 5.10723 5.3125 5.625C5.3125 6.14277 5.73223 6.5625 6.25 6.5625H23.75C24.2678 6.5625 24.6875 6.14277 24.6875 5.625C24.6875 5.10723 24.2678 4.6875 23.75 4.6875H18.4375V3.75C18.4375 3.23223 18.0178 2.8125 17.5 2.8125H12.5Z'
+									fill='red'
+								/>
+								<path
+									d='M12.5 13.3125C13.0178 13.3125 13.4375 13.7322 13.4375 14.25L13.4375 23C13.4375 23.5178 13.0178 23.9375 12.5 23.9375C11.9822 23.9375 11.5625 23.5178 11.5625 23L11.5625 14.25C11.5625 13.7322 11.9822 13.3125 12.5 13.3125Z'
+									fill='red'
+								/>
+								<path
+									d='M18.4375 14.25C18.4375 13.7322 18.0178 13.3125 17.5 13.3125C16.9822 13.3125 16.5625 13.7322 16.5625 14.25V23C16.5625 23.5178 16.9822 23.9375 17.5 23.9375C18.0178 23.9375 18.4375 23.5178 18.4375 23V14.25Z'
+									fill='red'
+								/>
+								<path
+									fillRule='evenodd'
+									clipRule='evenodd'
+									d='M7.48928 9.89647C7.54203 9.42169 7.94334 9.0625 8.42105 9.0625H21.5789C22.0566 9.0625 22.458 9.42169 22.5107 9.89647L22.7609 12.1482C23.2144 16.2298 23.2144 20.3491 22.7609 24.4308L22.7363 24.6525C22.5562 26.2727 21.3006 27.5646 19.6861 27.7906C16.5772 28.2258 13.4228 28.2258 10.3138 27.7906C8.69943 27.5646 7.44375 26.2727 7.26372 24.6525L7.23909 24.4308C6.78558 20.3491 6.78558 16.2298 7.23909 12.1482L7.48928 9.89647ZM9.26015 10.9375L9.10263 12.3552C8.6644 16.2992 8.6644 20.2797 9.10263 24.2237L9.12726 24.4454C9.21263 25.2138 9.80815 25.8265 10.5738 25.9337C13.5103 26.3448 16.4897 26.3448 19.4262 25.9337C20.1918 25.8265 20.7874 25.2138 20.8727 24.4454L20.8974 24.2237C21.3356 20.2797 21.3356 16.2993 20.8974 12.3552L20.7398 10.9375H9.26015Z'
+									fill='red'
+								/>
+							</svg>
+						</span>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Favorites;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,9 +5,12 @@ import './input.scss';
 import './loader.scss';
 import { useActions } from './../hooks/actions';
 import videoV from '../assets/VALORANT_ANNO22_SHATTERED_16x9_27s.mp4';
+import { useTransition, animated } from 'react-spring';
+import Favorites from './Favorites';
 
 const Input = () => {
-	const [nickname, setNickname] = useState('');
+	const [nickname, setNickname] = useState<string>('');
+	const [showFavorites, setShowFavorites] = useState<boolean>(false);
 	const { addData, reset, resetUnrated } = useActions();
 
 	const [fetchAccount, { isLoading, isError, data }] = useLazyGetAccDataQuery();
@@ -33,13 +36,13 @@ const Input = () => {
 	useEffect(() => {
 		resetUnrated();
 		reset();
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	useEffect(() => {
 		data?.name && addData(nickname);
 		data?.name && navigate('/account-details');
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [data]);
 
 	useEffect(() => {
@@ -48,6 +51,19 @@ const Input = () => {
 
 		username && tag && setNickname(`${username}#${tag}`);
 	}, []);
+
+	const transitionOptions = {
+		from: {
+			opacity: 0,
+		},
+		enter: {
+			opacity: 1,
+		},
+		leave: {
+			opacity: 0,
+		},
+	};
+	const transitionFavorites = useTransition(showFavorites, transitionOptions);
 
 	return (
 		<div className='input-wrapper'>
@@ -76,10 +92,39 @@ const Input = () => {
 			/>
 			<div style={{ position: 'relative' }}>
 				{isLoading && <span className='loader'></span>}
+				{!isLoading && (
+					<div
+						className='favorites-btn'
+						onClick={() => setShowFavorites(!showFavorites)}>
+						<svg
+							width='39'
+							height='39'
+							viewBox='0 0 39 39'
+							fill='none'
+							className='favorites-icon'
+							xmlns='http://www.w3.org/2000/svg'>
+							<path
+								fillRule='evenodd'
+								clipRule='evenodd'
+								d='M17.0403 6.68555C18.1363 4.79687 20.8637 4.79688 21.9597 6.68556L25.3159 12.4695C25.6252 13.0027 26.1599 13.3665 26.7694 13.4585L32.3426 14.3003C34.7719 14.6672 35.6241 17.7345 33.7321 19.302L29.6714 22.6662C29.1015 23.1384 28.8335 23.8833 28.9719 24.6104L30.1094 30.5838C30.555 32.9238 28.0986 34.7418 25.9909 33.632L20.4464 30.7126C19.854 30.4007 19.146 30.4007 18.5536 30.7126L13.0091 33.632C10.9014 34.7418 8.44504 32.9238 8.89061 30.5838L10.0281 24.6104C10.1665 23.8833 9.89854 23.1384 9.32859 22.6662L5.26787 19.302C3.37591 17.7345 4.22806 14.6672 6.65743 14.3003L12.2306 13.4585C12.8401 13.3665 13.3748 13.0027 13.6841 12.4695L17.0403 6.68555ZM19.8514 7.9089C19.6948 7.63909 19.3052 7.63909 19.1486 7.9089L15.7924 13.6929C15.1118 14.8658 13.9355 15.6662 12.5946 15.8687L7.02146 16.7105C6.6744 16.7629 6.55267 17.2011 6.82295 17.425L10.8837 20.7892C12.1376 21.8281 12.7272 23.4667 12.4226 25.0663L11.2851 31.0398C11.2214 31.3741 11.5723 31.6338 11.8734 31.4752L17.418 28.5558C18.7212 27.8696 20.2788 27.8696 21.582 28.5558L27.1266 31.4752C27.4277 31.6338 27.7786 31.3741 27.7149 31.0398L26.5774 25.0663C26.2728 23.4667 26.8624 21.8281 28.1163 20.7892L32.1771 17.425C32.4473 17.2011 32.3256 16.7629 31.9785 16.7105L26.4054 15.8687C25.0645 15.6662 23.8882 14.8658 23.2076 13.6929L19.8514 7.9089Z'
+								fill='#ff1d30'
+							/>
+						</svg>
+					</div>
+				)}
 				<button className='search-btn' onClick={searchHandler}>
 					<span className='search-text'>Search</span>
 				</button>
 			</div>
+ 
+			{transitionFavorites(
+				(style, item) =>
+					item && (
+						<animated.div style={style}>
+							<Favorites showFavorites={showFavorites} setShowFavorites={setShowFavorites} setNickname={setNickname}/>
+						</animated.div>
+					)
+			)}
 		</div>
 	);
 };
